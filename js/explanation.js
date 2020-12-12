46d648dc26321e1e6e8291428e7b38f8223d2771
yashyadav-create <70959046+yashyadav-create@users.noreply.github.com>
2020-12-12 15:22:09 +0530

    added some new 'cthullu' changes

---
+++ js/explanation.js
@@ -0,0 +1,46 @@
+var favcolor = "Blue";
+var favnumber = 1;
+var firstarray = [ 'blue', 'red', 'green'];
+
+var object ={
+firstName: 'Yash',
+LastName: 'Yadav',
+isMale:true
+
+};
+var tinku = function(){
+console.log("tinku pandita ban raha he.")
+
+}
+var addition=function(num1,num2){
+return num2+num1;
+
+}
+var num1=78;
+var num2=9;
+if(num1 > num2){
+alert('true');    
+}
+
+var cthullu ={
+legs:200,
+isDeadly:true,
+color:'LightSteelBlue',
+wings:[
+'wing1',
+'wing2'
+],
+appearance:'too abstract to be described by human perception',
+
+
+moving : function(){
+    this.isDeadly=false;
+},
+chasing : function(){
+    alert('Fuc*ing run');
+}
+
+};
+var s = function(naem){
+    alert("my name is "+naem)
+}
